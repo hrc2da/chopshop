de746e1bfbb6c418fb670accbf968b743716e073
mvl24 <mvl24@dhcp-rhodes-3214.redrover.cornell.edu>
2018-06-22 10:54:54 -0400

    changed plotting libraries and added a scatter plot

--- src/components/CostBenefitPlot.js
+++ src/components/CostBenefitPlot.js
@@ -1,4 +1,5 @@
-import { ResponsiveScatterPlot } from '@nivo/scatterplot';
+import { ScatterPlot,ResponsiveScatterPlot } from '@nivo/scatterplot';
+import { Scatter } from 'react-chartjs-2';
 import React from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/Table';
@@ -6,56 +7,90 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
+export const adjustReward = (result)=>{
+  const rewardScale = 1.0;
+  const gasScale = 1.0/1e9;
+  const grassScale = 1.0;
+  return [rewardScale*result[0],gasScale*result[1],grassScale*result[2]];
+}
+
+
 
 let CostBenefitPlot = ({testedCars,gaCars})=>{
   console.log("CARS",testedCars);
-  return <React.Fragment>
-    <ResponsiveScatterPlot
-  //width={600}
- // height={400}
+  let cars = [];
+  let labels = [];
+  if(testedCars.length>0){
+    cars.push({
+      label: 'My Cars',
+      data: testedCars.map(((car,index)=>{return {id:index, x: car['result'][0], y: car['result'][1]};})),
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderWidth: 5,
+      pointHoverRadius: 20,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 10,
+      pointRadius: 10,
+      pointHitRadius: 50,
+    });
+    labels.push('My Cars');
+  }
+  if(gaCars.length>0){
+    cars.push({
+      label: 'Computer-Generated Cars',
+      data: gaCars.map(((car,index)=>{return {id:index, x: car['result'][0], y: car['result'][1]};}))
+    });
+    labels.push('Computer-Generated Cars');
+  }
+  const data = {
+    labels:labels,
+    datasets:cars
+  }
+  console.log(cars);
+  return <div>
+  <Scatter data={data} />
+</div> 
+};
+  /*return <React.Fragment>
+    <div height={450}>
+    <ScatterPlot
+ height={250}
+ width={400}
   margin={{
-        top: 60,
-          right: 80,
-          bottom: 80,
-          left: 80
+        top: 10,
+          right: 10,
+          bottom: 10,
+          left: 20
       }}
+      enableGridX={true}
+      enableGridY={true}
   symbolSize={10}
   axisBottom={{
-    "orient": "bottom",
-    "tickSize": 5,
-    "tickPadding": 5,
-    "tickRotation": 0,
-    "legend": "cost",
-    "legendPosition": "center",
-    "legendOffset":46
-  }}
-  axisLeft={{
     orient: "bottom",
     tickSize: 5,
     tickPadding: 5,
     tickRotation: 0,
-    legend: "weight",
-    legendPostion: "center",
-    legendOffset: 46
+    legend: "cost",
+    legendPosition: "bottom",
+    legendOffset:5
   }}
-  data={[
-        {id: 'My Cars',data: [
-                  {id: 0,x: 0,y: 2},
-                  {id: 1,x: 1,y: 2.1},
-                  {id: 2,x: 2,y: 2.3},
-                ]},
-        {id: 'Computer-Generated Cars',data: [
-                  {id: 0,x: 0,y: 3.2},
-                  {id: 1,x: 3,y: 3.3},
-                  {id: 2,x: 5,y: 3.6},
-                ]},
-      ]}
+  axisLeft={{
+    orient: "left",
+    tickSize: 5,
+    tickPadding: 5,
+    tickRotation: 0,
+    legend: "weight",
+    legendPostion: "left",
+    legendOffset:-5 
+  }}
+  data={cars}
   legends={[{
           anchor: 'bottom-left',
           direction: 'row',
           translateY: 60,
         }]}
-/>
+/></div>
     <Table>
     <TableHead><TableCell>Tested Cars</TableCell></TableHead>
         {testedCars.map(car=>{
@@ -69,5 +104,5 @@ let CostBenefitPlot = ({testedCars,gaCars})=>{
         </TableRow>})}
     </Table>
     </React.Fragment>
-  }
+  }*/
 export default CostBenefitPlot;
